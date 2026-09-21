3ebb60a13a573d28f40f5b7a900af6e1ea73d7b4
Shxrpzoo <166571639+Shxrpzoo@users.noreply.github.com>
2026-09-22 00:13:15 +0100

    Delete scripts directory

--- scripts/assets.ts
+++
@@ -1,7 +0,0 @@
-import { access } from "node:fs/promises";
-import { seedCatalog } from "../lib/catalog";
-for (const item of seedCatalog.items)
-  await access("public" + decodeURIComponent(item.image));
-console.log(
-  `All ${seedCatalog.items.length} supplied image references exist, with exact filename case.`,
-);
